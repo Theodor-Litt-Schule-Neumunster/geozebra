42c14e7ce3f6fc5d1b0fb1952898e26d31d42590
RealKnoblauchbrot <bobecasimirpercy@gmail.com>
2025-01-31 10:20:47 +0100

    Refactor: Refactor Settings_service and rename it.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:geozebra_app/services/async_service.dart';
+import 'package:geozebra_app/services/settings_service.dart';
 // import 'package:geozebra_app/screens/practice_screen.dart';
 
 import '../widgets/bottombar_widget.dart';
@@ -20,7 +20,7 @@ class _HomeScreen extends State<HomeScreen> {
   String _savedName = "Der Jens";
 
   Future<String> _userName() async {
-    _savedName = await SettingsService.loadSetting<String>("displayname", "Der Jens");
+    _savedName = await SettingsService().getValue<String>("displayname", "Der Jens");
     setState(() {});
 
     return _savedName;
