4a23c045d35c75af754dec4cf90c9ce3449a392d
crooky2 <steini123bach@gmail.com>
2025-03-02 08:13:57 +0100

    Add test lesson and implement default lesson screen components

--- lib/screens/lesson_screen.dart
+++ lib/screens/lesson_screen.dart
@@ -5,8 +5,9 @@ import 'package:webview_flutter/webview_flutter.dart';
 import '../models/class_model.dart';
 import '../widgets/defaultappbar_widget.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'package:geozebra_app/models/theme_model.dart';
+import '../widgets/lessonScreen/defaultBody.dart';
+import '../widgets/lessonScreen/defaultDrawer.dart';
 
 class LessonScreen extends StatefulWidget {
   final Lesson lesson;
@@ -135,123 +136,16 @@ class _LessonScreenState extends State<LessonScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DefaultAppBar(title: widget.lesson.lessonTitle),
-      endDrawer: Builder(
-        builder: (context) => Drawer(
-          child: ListView(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text("Aufgaben", style: TextStyle(fontSize: 22)),
-                  ),
-                  ListTile(
-                    title: Text("Desktop Modus"),
-                    trailing: Transform.scale(
-                      scale: 0.8,
-                      child: Switch(
-                        value: false,
-                        onChanged: (bool value) {
-                          // Handle switch state change
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-                const Padding(
-                padding: EdgeInsets.only(bottom: 12.0),
-                child: Divider(
-                  height: 2,
-                  thickness: 2,
-                  color: Colors.grey,
-                ),
-                ),
-              ...widget.lesson.tasks.map((task) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 6.0, vertical: 2.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: ExpansionTile(
-                      title: Text(task.shortDescription),
-                      tilePadding: EdgeInsets.symmetric(horizontal: 16.0),
-                      backgroundColor: taskStatus[task.id] == true
-                          ? Theme.of(context)
-                              .extension<TaskColors>()!
-                              .completedTask
-                          : Theme.of(context)
-                              .extension<TaskColors>()!
-                              .uncompletedTask,
-                      collapsedBackgroundColor: taskStatus[task.id] == true
-                          ? Theme.of(context)
-                              .extension<TaskColors>()!
-                              .completedTask
-                          : Theme.of(context)
-                              .extension<TaskColors>()!
-                              .uncompletedTask,
-                      children: [
-                        ListTile(
-                          title: Text(task.description),
-                          onLongPress: () {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("Task Details"),
-                                  content: Text(task.description),
-                                  actions: [
-                                    TextButton(
-                                      child: Text("Close"),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              })
-            ],
-          ),
-        ),
-      ),
-      body: Stack(
-        children: [
-          WebViewWidget(controller: _controller),
-          if (isLoading)
-            FadeTransition(
-              opacity: _fadeAnimation,
-              child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.calculate_rounded,
-                          size: 80, color: Colors.blueAccent),
-                      SizedBox(height: 20),
-                      Text("Loading GeoGebra...",
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold)),
-                      SizedBox(height: 20),
-                      CircularProgressIndicator(
-                          strokeWidth: 3, color: Colors.blueAccent),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-        ],
-      ),
+      endDrawer: widget.lesson.showRechner
+          ? DefaultDrawer(lesson: widget.lesson, taskStatus: taskStatus)
+          : null,
+      body: widget.lesson.showRechner
+          ? DefaultBody(
+              controller: _controller,
+              isLoading: isLoading,
+              fadeAnimation: _fadeAnimation,
+            )
+          : Center(child: Text("Rechner is not available for this lesson")),
     );
   }
 }
-
