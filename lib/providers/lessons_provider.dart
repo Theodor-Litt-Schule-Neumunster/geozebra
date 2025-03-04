58c3d35a9d4d6db96b788fe8748a710439544815
Marvin <alibu1.mw57@gmail.com>
2025-03-04 13:11:45 +0100

    change tasks: split up tasks with multiple soloutions; make checking of fractions possible;

--- lib/providers/lessons_provider.dart
+++ lib/providers/lessons_provider.dart
@@ -1,3 +1,5 @@
+import '../utils/task_validator.dart';
+
 class LessonsProvider {
   List<String> classFiles = [
     'basic_geogebra',
@@ -13,5 +15,10 @@ class LessonsProvider {
 
   List<String> getAllClassFiles() {
     return classFiles.map((file) => 'assets/classes/$file.json').toList();
+  }
+  
+  /// Validates whether a user's solution completes a task
+  bool isTaskCompleted(Map<String, dynamic> taskCondition, Map<String, dynamic> userSolution) {
+    return TaskValidator.isTaskCompleted(taskCondition, userSolution);
   }
 }
